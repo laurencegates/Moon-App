eb21572aed689f647085a9b5402ed7edb9a93ddb
Laurence Gates <laurence.gates@koretechx.com>
2023-11-01 16:16:10 +0500

    fourth release

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:moon_app/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
